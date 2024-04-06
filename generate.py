61ace9f4508c2bf4065b35538059c60417bd98d6
matthieuEv <evainmatthieu44@gmail.com>
2024-04-06 10:26:20 +0200

    add image control

--- generate.py
+++ generate.py
@@ -2,7 +2,41 @@ from jinja2 import Environment, FileSystemLoader
 import json
 from pdflatex import PDFLaTeX
 
+import urllib.request
+import os
+import json
+
+import os
+import urllib.request
+
+def download_images(data, img_count=0, img_dir='images'):
+    if isinstance(data, dict):
+        for key, value in data.items():
+            if key == 'image':
+                print("image found")
+                img_count += 1
+                img_path = os.path.abspath(os.path.join(img_dir, f'img{img_count}.png')).replace("\\", "/")
+                urllib.request.urlretrieve(value, img_path)
+                print("data[key]: ",data[key])
+                data[key] = img_path
+                print("data[key]: ",data[key])
+                print("--")
+            else:
+                data[key], img_count = download_images(value, img_count, img_dir)
+    elif isinstance(data, list):
+        for i in range(len(data)):
+            data[i], img_count = download_images(data[i], img_count, img_dir)
+    return data, img_count
+
+
 def generatePDF(data,lang):
+    if not os.path.exists('images'):
+        os.makedirs('images')
+    # Download images and update the data
+    data, _ = download_images(data)
+
+    print("data: ",data)
+
     custom_commands = "\\newcommand{\\resumeItem}[1]{  \\item\\small{    {#1 \\vspace{-1pt}}  }}\\newcommand{\\classesList}[4]{    \\item\\small{        {#1 #2 #3 #4 \\vspace{-2pt}}  }}\\newcommand{\\resumeSubheading}[4]{  \\vspace{-2pt}\\item    \\begin{tabular*}{1.0\\textwidth}[t]{l@{\\extracolsep{\\fill}}r}      \\textbf{\\large#1} & \\textbf{\\small #2} \\\\      \\textit{\\large#3} & \\textit{\\small #4} \\\\          \\end{tabular*}\\vspace{-7pt}}\\newcommand{\\resumeSingleSubheading}[4]{  \\vspace{-2pt}\\item    \\begin{tabular*}{1.0\\textwidth}[t]{l@{\\extracolsep{\\fill}}r}      \\textbf{\\large#1} & \\textbf{\\small #2} \\\\          \\end{tabular*}\\vspace{-7pt}}\\newcommand{\\resumeSubSubheading}[2]{    \\item    \\begin{tabular*}{0.97\\textwidth}{l@{\\extracolsep{\\fill}}r}      \\textit{\\small#1} & \\textit{\\small #2} \\\\    \\end{tabular*}\\vspace{-7pt}}\\newcommand{\\resumeProjectHeading}[2]{    \\item    \\begin{tabular*}{1.001\\textwidth}{l@{\\extracolsep{\\fill}}r}      \\small#1 & \\textbf{\\small #2}\\\\    \\end{tabular*}\\vspace{-7pt}}\\newcommand{\\resumeSubItem}[1]{\\resumeItem{#1}\\vspace{-4pt}}\\renewcommand\\labelitemi{$\\vcenter{\\hbox{\\tiny$\\bullet$}}$}\\renewcommand\\labelitemii{$\\vcenter{\\hbox{\\tiny$\\bullet$}}$}\\newcommand{\\resumeSubHeadingListStart}{\\begin{itemize}[leftmargin=0.0in, label={}]}\\newcommand{\\resumeSubHeadingListEnd}{\\end{itemize}}\\newcommand{\\resumeItemListStart}{\\begin{itemize}[leftmargin=0.1in]}\\newcommand{\\resumeItemListEnd}{\\end{itemize}\\vspace{-5pt}}\\newcommand\\sbullet[1][.5]{\\mathbin{\\vcenter{\\hbox{\\scalebox{#1}{$\\bullet$}}}}}"
     data["custom_commands"] = custom_commands
 
